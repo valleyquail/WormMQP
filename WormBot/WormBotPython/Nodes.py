b0a2eff4800fcf2162560b55630bae6c86296492
shiv-sirsiwal <ssirsiwal@wpi.edu>
2025-02-18 14:37:03 -0500

    fk is almost there, the base needs to be fixed and some parts are squishing weird

--- WormBot/WormBotPython/Nodes.py
+++ WormBot/WormBotPython/Nodes.py
@@ -2,9 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from multipledispatch import dispatch
 
-
 class Node:
-
     def __init__(self, initial_x: float, initial_y: float, initial_z: float,
                  node_level: int, node_type: str = None, node_id=None):
         self.x: float = initial_x
@@ -32,19 +30,20 @@ class Node:
         self.y = y
         self.z = z
 
-    @dispatch(tuple[float, float, float])
-    def set_position(self, pos: tuple[float, float, float]) -> None:
-        self.x, self.y, self.z = pos[0]
+    @dispatch(tuple)  # Changed from tuple[float, float, float] to just tuple
+    def set_position(self, pos: tuple) -> None:
+        # Added validation and fixed the indexing
+        if len(pos) != 3:
+            raise ValueError("Position tuple must have exactly 3 elements")
+        self.x, self.y, self.z = pos  # Removed [0] as pos is already the tuple
 
-    def __str__(self):
+    def __str__(self):  # Fixed the double underscores
         return f"Node id {self.id}, level {self.level}, type {self.type})"
-
 
 class MidpointNode(Node):
     def __init__(self, initial_x: float, initial_y: float, initial_z: float, node_level: int):
         super().__init__(initial_x, initial_y, initial_z, node_level)
         self.type = 'midpoint'
-
 
 class VertexNode(Node):
     def __init__(self, initial_x: float, initial_y: float, initial_z: float, node_level: int):
